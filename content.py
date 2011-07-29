ce234bbcd43ca9b7b64e19afb5e220774db2a292
Frederik Cornillie <frederik.cornillie@gmail.com>
2011-07-29 15:25:42 +0200

    scheduler enhanced

---
+++ content.py
@@ -0,0 +1,92 @@
+# This Python file uses the following encoding: utf-8
+
+recipes = [
+	{
+		'name':'fruitbegnets',
+		'teaser':'Lekkere vettigheid voor een zomerse herfstdag.',
+		'type':'dessert',
+		'preparation_time':30,
+		'cooking_time':50,
+		'foodtypes':(),
+		'image':'http://www.vt4.be/files/imagecache/recept_full/recepten/GG13_Fruitbeignets.jpg',
+		'season':'',
+		'publish':'public',
+		'ingredients':u'''250 ml (bruin) bier
+140 g bloem
+1 eidooier
+4 eiwitten
+20 g maïsolie
+20 g suiker
+snuifje zout
+
+appels
+ananas
+banaan
+
+frituurpan
+plantaardige, neutrale frituurolie om te bakken''',
+		'method':u'''Zeef de bloem in een ruime kom en voeg er een snuifje zout aan toe.
+Voeg er de eidooier, bier, en olie bij en roer tot een glad beslag zonder klonters.
+Laat het beslag een halfuurtje afgedekt rusten.
+
+Snij intussen het fruit in stukken.
+Klop net voor het bakken de eiwitten en suiker in pieken. Spatel dit voorzichtig onder het beslag.
+
+Verwarm de frituurpan (met plantaardige olie) op 180°C, haal het fruit door het beslag en bak in 3 minuten goudgeel.
+Laat even uitlekken op keukenpapier, bestrooi met poedersuiker en serveer meteen.
+
+TIP:
+Het bier kan vervangen worden door 250 ml melk en 20 g gist.
+
+Met gist, bier of mineraalwater wordt het beslag luchtig. Met wijn worden de beignets knapperiger door het aanwezige wijnsteenzuur.
+
+Een beslag met bier is sneller klaar maar blijft na het bakken niet zo lang krokant. Een gistbeslag daarentegen blijft langer krokant maar moet van tevoren kunnen rijzen.
+
+Voor de bananenbeignets, eerst de gesneden banaan(stukjes) marineren in wat griessuiker, citroensap en rum. Extra lekker! ''',
+	},
+	{
+		'name':'uiensoep met appeltjes',
+		'teaser':'Lekker stinken achterna!',
+		'type':'starter',
+		'preparation_time':25,
+		'cooking_time':60,
+		'foodtypes':('soep',),
+		'image':'http://www.vt4.be/files/imagecache/recept_full/recepten/Seabelle%20voorgerecht.jpg',
+		'season':'',
+		'publish':'public',
+		'ingredients':u'''- 5 dikke uien
+- 1 aardappel
+- 3 jonagold appelen
+- 1,5 kippenbouillon
+- peper en zout''',
+		'method':u'''Snijd de uien, de aardappel en 2,5 appelen in grove stukken.
+Stoof de uien en de aardappelen glazig en doe dan de appelen erbij, samen met de bouillon.
+Laat 15 minuten doorkoken en mix dan de soep.
+Snijd de overgebleven halve appel in stukjes en bak ze goudbruin in de pan.
+Doe de gebakken appeltjes in een bord en giet de soep erover.''',
+	},
+	{
+		'name':'Springbok en lotte met pasta, wokgroentjes en een botersaus met rode wijn',
+		'teaser':'Jumpen!',
+		'type':'main',
+		'preparation_time':90,
+		'cooking_time':60,
+		'foodtypes':('vis',),
+		'image':'http://www.vt4.be/files/imagecache/recept_full/recepten/Peter%20hoofdgerecht.jpg',
+		'season':'',
+		'publish':'public',
+		'ingredients':u'''- 4 stukken springbok filet van ca.120 gr
+- 500 gr filet van lotte
+- kruiden
+- peper en zout
+- bloem
+- 1 sjalotje
+- 100 ml rode wijn
+- tijm
+- 250 ml wildfond
+- 100 gr roomboter''',
+		'method':u'''Snijd de lotte in stukken en dep hem droog. Rol alle stukjes in bloem en bak ze kort aan in een wokpan. Bak de springbok in een beetje olijfolie en kruid met peper en zout.
+Versnipper het sjalotje en stoof het aan in wat boter. Blus met rode wijn en kruid met tijm, peper en zout. Laat zacht karamelliseren en blus dan met de wildfond. Laat inkoken.
+Voeg dan de boter in kleine blokjes toe en zorg ervoor dat de saus niet meer kookt.''',
+	},
+]
