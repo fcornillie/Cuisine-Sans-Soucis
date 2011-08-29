fa1b77bc22ef94ece94268e545176c0a5a0d7450
Frederik Cornillie <frederik.cornillie@gmail.com>
2011-08-29 21:36:42 +0200

    migrated to yarbapp

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,1 +1,11 @@
 webapp_django_version = '1.2'
+
+#########################################
+# Remote_API Authentication configuration.
+#
+# See google/appengine/ext/remote_api/handler.py for more information.
+# For datastore_admin datastore copy, you should set the source appid
+# value.  'HTTP_X_APPENGINE_INBOUND_APPID', ['trusted source appid here']
+#
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
+    'HTTP_X_APPENGINE_INBOUND_APPID', ['cuisinesanssoucis'])
